88ceccd672f695ac583ce571aec4b9d8c800c279
haruka-210-s <waitforthehaze1108@icloud.com>
2025-11-26 13:41:44 +0900

    プレイヤーのジャンプを修正しました

--- player.h
+++ player.h
@@ -15,7 +15,7 @@ using namespace DirectX;
 #define GENSUI (0.98f)
 #define STOP_VELO (0.0002f)
 #define PLAYER_ACCELERATION (0.01f)  //加速度
-#define PLAYER_GRAVITY (0.02f)       //重力
+#define PLAYER_GRAVITY (0.01f)       //重力
 #define PLAYER_JUMP (0.3f)           //ジャンプ力
 #define PLAYER_DEATH (-3.0f)         //これ以上下に行くと死
 #define PLAYER_MAGICRANGE (0.03f)    //魔法範囲
@@ -25,6 +25,7 @@ enum PLAYER_STATE
 {
 	PLAYER_STATE_IDLE = 0,	//何もしない
 	PLAYER_STATE_MOVE,		//移動
+	PLAYER_STATE_JUMP,
 	PLAYER_STATE_RESPAWN,   //リスポーン
 	
 };
@@ -49,6 +50,7 @@ private:
 private:
 	void	Player_Idle();
 	void	Player_Move();
+	void    Player_Jump();
 	void    Player_BombMagic(BOMBSOURCE* pBomb);
 	void    Player_Respawn();
 
