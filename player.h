e870e5622ae3f9f7c3295e0e683cfbd325bee697
haruka-210-s <waitforthehaze1108@icloud.com>
2025-11-27 19:06:14 +0900

    落下速度に制限をつけました

--- player.h
+++ player.h
@@ -16,7 +16,8 @@ using namespace DirectX;
 #define STOP_VELO (0.0002f)
 #define PLAYER_ACCELERATION (0.01f)  //加速度
 #define PLAYER_GRAVITY (0.01f)       //重力
-#define PLAYER_JUMP (0.3f)           //ジャンプ力
+#define PLAYER_FALLMAX (-0.2f)        //落下最高速度
+#define PLAYER_JUMP (0.2f)           //ジャンプ力
 #define PLAYER_DEATH (-3.0f)         //これ以上下に行くと死
 #define PLAYER_MAGICRANGE (0.03f)    //魔法範囲
 
