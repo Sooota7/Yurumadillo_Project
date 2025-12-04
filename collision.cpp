e4f5a4e43dacf0ac196d442e03e1eadf00637429
tolutsu823 <ouynl56773@yahoo.co.jp>
2025-12-04 19:07:57 +0900

    no message

--- collision.cpp
+++ collision.cpp
@@ -92,7 +92,7 @@ float	COLLISION::PlayerFieldCollision(PLAYER* pPlayer, MAPDATA* pField)
 						PlayerPos.y > BoxTop)
 					{//BOX‚Ì+X–Ê‚É‚Ô‚Â‚©‚Á‚½
 						PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
-						PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
+						PlayerVel.y = 0;//PlayerVel.y * (-COE * 1.0f);
 						hit = COLLISION_HIT::HIT_GROUND;
 						if (PlayerJump == false)
 						{
