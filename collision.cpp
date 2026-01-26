ec5a4859e175df88165811d7b8211d290ccbbaf8
FFK-37 <fu6ji25kai@gmail.com>
2026-01-26 15:06:32 +0900

    アニメーション追加

--- collision.cpp
+++ collision.cpp
@@ -453,348 +453,377 @@ float	COLLISION::PlayerBombCollision(PLAYER* pPlayer, BOMB* pBomb)
 	RUNBOMBSOURCE* RunBomb = pBomb->Bomb_GetRunBomb();// マップ
 	FLOWTBOMBSOURCE* FlowtBomb = pBomb->Bomb_GetFlowtBomb();// マップ
 	//int			i = 0;
-
+	bool bombHave = false;
 
 	// 全てのブロックをチェック
-
-	
+	//Have判定
 	for (int i = 0; i < BOMB_NUM_MAX; i++)
+	{//ボム
+		if (Bomb[i].BombSource_GetState()==BOMB_STATE::BOMB_ACTIVE_HAVE)
+		{
+			bombHave = true;
+		}
+	}
+	for (int i = 0; i < BOMB_NUM_MAX; i++)
+	{//ランボム
+		if (RunBomb[i].Runbombsource_GetState()==BOMB_STATE::BOMB_ACTIVE_HAVE)
+		{
+			bombHave = true;
+		}
+	}
+	for (int i = 0; i < BOMB_NUM_MAX; i++)
+	{//浮きボム
+		if (FlowtBomb[i].Flowtbombsource_GetState()==BOMB_STATE::BOMB_ACTIVE_HAVE)
+		{
+			bombHave = true;
+		}
+	}
+
+	if (bombHave)
 	{
-		/*switch (Bomb[i].BombSource_GetState())
+		
+	}
+	else
+	{
+		for (int i = 0; i < BOMB_NUM_MAX; i++)
 		{
-		case BOMB_NONE:
-		case BOMB_MAX:
-		default:
-			continue;
-			break;
-		}*/
-
-		bool	touch = false;
-		float BoxTop;	// BOXの+Y面の座標
-
-		XMFLOAT3 bombPos;
-
-
-		bombPos = Bomb[i].BombSource_GetPosition();
-
-
-
-		switch (Bomb[i].BombSource_GetState())
-		{
-
-		default:
-			BoxTop = bombPos.y + BOMB_RADIUS;	// 普通のBOX
-			break;
-		}
-
-		// 壁としての判定処理
-		if (bombPos.y - BOMB_RADIUS <= PlayerPos.y &&
-			PlayerPos.y <= BoxTop - 0.1f)
-		{
-			if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
-				PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+			/*switch (Bomb[i].BombSource_GetState())
 			{
-				if (bombPos.x - BOMB_RADIUS <= PlayerPos.x + PLAYER_RADIUS &&
-					PlayerPos.x <= bombPos.x - BOMB_RADIUS)
-				{//BOXの-X面にぶつかったので座標の補正
-					PlayerPos.x += (bombPos.x - BOMB_RADIUS) - (PlayerPos.x + PLAYER_RADIUS);
-					PlayerVel.x *= -COE; //移動ベクトルの反転
-					hit = COLLISION_HIT::HIT_WALL_3;
-					touch = true;
-				}
-				else if (bombPos.x + BOMB_RADIUS >= PlayerPos.x - PLAYER_RADIUS &&
-					PlayerPos.x >= bombPos.x + BOMB_RADIUS)
-				{//BOXの+X面にぶつかった
-					PlayerPos.x += (bombPos.x + BOMB_RADIUS) - (PlayerPos.x - PLAYER_RADIUS);
-					PlayerVel.x *= -COE;
-					hit = COLLISION_HIT::HIT_WALL_1;
-					touch = true;
-				}
+			case BOMB_NONE:
+			case BOMB_MAX:
+			default:
+				continue;
+				break;
+			}*/
+
+			bool	touch = false;
+			float BoxTop;	// BOXの+Y面の座標
+
+			XMFLOAT3 bombPos;
+
+
+			bombPos = Bomb[i].BombSource_GetPosition();
+
+
+
+			switch (Bomb[i].BombSource_GetState())
+			{
+
+			default:
+				BoxTop = bombPos.y + BOMB_RADIUS;	// 普通のBOX
+				break;
 			}
-			else if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
-				PlayerPos.x <= bombPos.x + BOMB_RADIUS)
+
+			// 壁としての判定処理
+			if (bombPos.y - BOMB_RADIUS <= PlayerPos.y &&
+				PlayerPos.y <= BoxTop - 0.1f)
 			{
-				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z + PLAYER_RADIUS &&
-					PlayerPos.z <= bombPos.z - BOMB_RADIUS)
-				{//BOXの-Z面にぶつかったので座標の補正
-					PlayerPos.z += (bombPos.z - BOMB_RADIUS) - (PlayerPos.z + PLAYER_RADIUS);
-					PlayerVel.z *= -COE; //移動ベクトルの反転
-					hit = COLLISION_HIT::HIT_WALL_0;
-					touch = true;
+				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
+					PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+				{
+					if (bombPos.x - BOMB_RADIUS <= PlayerPos.x + PLAYER_RADIUS &&
+						PlayerPos.x <= bombPos.x - BOMB_RADIUS)
+					{//BOXの-X面にぶつかったので座標の補正
+						PlayerPos.x += (bombPos.x - BOMB_RADIUS) - (PlayerPos.x + PLAYER_RADIUS);
+						PlayerVel.x *= -COE; //移動ベクトルの反転
+						hit = COLLISION_HIT::HIT_WALL_3;
+						touch = true;
+					}
+					else if (bombPos.x + BOMB_RADIUS >= PlayerPos.x - PLAYER_RADIUS &&
+						PlayerPos.x >= bombPos.x + BOMB_RADIUS)
+					{//BOXの+X面にぶつかった
+						PlayerPos.x += (bombPos.x + BOMB_RADIUS) - (PlayerPos.x - PLAYER_RADIUS);
+						PlayerVel.x *= -COE;
+						hit = COLLISION_HIT::HIT_WALL_1;
+						touch = true;
+					}
 				}
-				else if (bombPos.z + BOMB_RADIUS >= PlayerPos.z - PLAYER_RADIUS &&
-					PlayerPos.z >= bombPos.z + BOMB_RADIUS)
-				{//BOXの+Z面にぶつかった
-					PlayerPos.z += (bombPos.z + BOMB_RADIUS) - (PlayerPos.z - PLAYER_RADIUS);
-					PlayerVel.z *= -COE;
-					hit = COLLISION_HIT::HIT_WALL_2;
-					touch = true;
-				}
-			}
-		}
-		//地面として判定処理
-		else
-		{
-			if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
-				PlayerPos.z <= bombPos.z + BOMB_RADIUS)
-			{
-				if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
+				else if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
 					PlayerPos.x <= bombPos.x + BOMB_RADIUS)
 				{
-					if (bombPos.y - BOMB_RADIUS <= PlayerPos.y + PLAYER_RADIUS &&
-						PlayerPos.y <= bombPos.y - BOMB_RADIUS)
-					{//BOXの-X面にぶつかったので座標の補正
-						PlayerPos.y += (bombPos.y - BOMB_RADIUS) - (PlayerPos.y + PLAYER_RADIUS);
-						PlayerVel.y *= -COE; //移動ベクトルの反転
-						//hit = 
+					if (bombPos.z - BOMB_RADIUS <= PlayerPos.z + PLAYER_RADIUS &&
+						PlayerPos.z <= bombPos.z - BOMB_RADIUS)
+					{//BOXの-Z面にぶつかったので座標の補正
+						PlayerPos.z += (bombPos.z - BOMB_RADIUS) - (PlayerPos.z + PLAYER_RADIUS);
+						PlayerVel.z *= -COE; //移動ベクトルの反転
+						hit = COLLISION_HIT::HIT_WALL_0;
+						touch = true;
 					}
-					else if (BoxTop >= PlayerPos.y - PLAYER_RADIUS &&
-						PlayerPos.y >= BoxTop)
-					{//BOXの+X面にぶつかった
-						PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
-						PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
-						hit = COLLISION_HIT::HIT_GROUND;
-						if (PlayerJump == false)
-						{
-							PlayerJump = true;
+					else if (bombPos.z + BOMB_RADIUS >= PlayerPos.z - PLAYER_RADIUS &&
+						PlayerPos.z >= bombPos.z + BOMB_RADIUS)
+					{//BOXの+Z面にぶつかった
+						PlayerPos.z += (bombPos.z + BOMB_RADIUS) - (PlayerPos.z - PLAYER_RADIUS);
+						PlayerVel.z *= -COE;
+						hit = COLLISION_HIT::HIT_WALL_2;
+						touch = true;
+					}
+				}
+			}
+			//地面として判定処理
+			else
+			{
+				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
+					PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+				{
+					if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
+						PlayerPos.x <= bombPos.x + BOMB_RADIUS)
+					{
+						if (bombPos.y - BOMB_RADIUS <= PlayerPos.y + PLAYER_RADIUS &&
+							PlayerPos.y <= bombPos.y - BOMB_RADIUS)
+						{//BOXの-X面にぶつかったので座標の補正
+							PlayerPos.y += (bombPos.y - BOMB_RADIUS) - (PlayerPos.y + PLAYER_RADIUS);
+							PlayerVel.y *= -COE; //移動ベクトルの反転
+							//hit = 
+						}
+						else if (BoxTop >= PlayerPos.y - PLAYER_RADIUS &&
+							PlayerPos.y >= BoxTop)
+						{//BOXの+X面にぶつかった
+							PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
+							PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
+							hit = COLLISION_HIT::HIT_GROUND;
+							if (PlayerJump == false)
+							{
+								PlayerJump = true;
+							}
 						}
 					}
 				}
 			}
+
+			Bomb[i].BombSource_SetTouch(touch);
+			pPlayer->SetPlayerJump(PlayerJump);
+			pPlayer->SetPlayerPosition(PlayerPos);
+			pPlayer->SetPlayerVelocity(PlayerVel);
+
 		}
 
-		Bomb[i].BombSource_SetTouch(touch);
-		pPlayer->SetPlayerJump(PlayerJump);
-		pPlayer->SetPlayerPosition(PlayerPos);
-		pPlayer->SetPlayerVelocity(PlayerVel);
-
-	}
-	
-	for (int i = 0; i < BOMB_NUM_MAX; i++)
-	{
-		/*switch (Bomb[i].BombSource_GetState())
+		for (int i = 0; i < BOMB_NUM_MAX; i++)
 		{
-		case BOMB_NONE:
-		case BOMB_MAX:
-		default:
-			continue;
-			break;
-		}*/
-
-		bool	touch = false;
-		float BoxTop;	// BOXの+Y面の座標
-
-		XMFLOAT3 bombPos;
-
-
-		bombPos = RunBomb[i].Runbombsource_GetPosition();
-
-
-
-		switch (RunBomb[i].Runbombsource_GetState())
-		{
-
-		default:
-			BoxTop = bombPos.y + BOMB_RADIUS;	// 普通のBOX
-			break;
-		}
-
-		// 壁としての判定処理
-		if (bombPos.y - BOMB_RADIUS <= PlayerPos.y &&
-			PlayerPos.y <= BoxTop - 0.1f)
-		{
-			if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
-				PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+			/*switch (Bomb[i].BombSource_GetState())
 			{
-				if (bombPos.x - BOMB_RADIUS <= PlayerPos.x + PLAYER_RADIUS &&
-					PlayerPos.x <= bombPos.x - BOMB_RADIUS)
-				{//BOXの-X面にぶつかったので座標の補正
-					PlayerPos.x += (bombPos.x - BOMB_RADIUS) - (PlayerPos.x + PLAYER_RADIUS);
-					PlayerVel.x *= -COE; //移動ベクトルの反転
-					hit = COLLISION_HIT::HIT_WALL_3;
-					touch = true;
-				}
-				else if (bombPos.x + BOMB_RADIUS >= PlayerPos.x - PLAYER_RADIUS &&
-					PlayerPos.x >= bombPos.x + BOMB_RADIUS)
-				{//BOXの+X面にぶつかった
-					PlayerPos.x += (bombPos.x + BOMB_RADIUS) - (PlayerPos.x - PLAYER_RADIUS);
-					PlayerVel.x *= -COE;
-					hit = COLLISION_HIT::HIT_WALL_1;
-					touch = true;
-				}
+			case BOMB_NONE:
+			case BOMB_MAX:
+			default:
+				continue;
+				break;
+			}*/
+
+			bool	touch = false;
+			float BoxTop;	// BOXの+Y面の座標
+
+			XMFLOAT3 bombPos;
+
+
+			bombPos = RunBomb[i].Runbombsource_GetPosition();
+
+
+
+			switch (RunBomb[i].Runbombsource_GetState())
+			{
+
+			default:
+				BoxTop = bombPos.y + BOMB_RADIUS;	// 普通のBOX
+				break;
 			}
-			else if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
-				PlayerPos.x <= bombPos.x + BOMB_RADIUS)
+
+			// 壁としての判定処理
+			if (bombPos.y - BOMB_RADIUS <= PlayerPos.y &&
+				PlayerPos.y <= BoxTop - 0.1f)
 			{
-				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z + PLAYER_RADIUS &&
-					PlayerPos.z <= bombPos.z - BOMB_RADIUS)
-				{//BOXの-Z面にぶつかったので座標の補正
-					PlayerPos.z += (bombPos.z - BOMB_RADIUS) - (PlayerPos.z + PLAYER_RADIUS);
-					PlayerVel.z *= -COE; //移動ベクトルの反転
-					hit = COLLISION_HIT::HIT_WALL_0;
-					touch = true;
+				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
+					PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+				{
+					if (bombPos.x - BOMB_RADIUS <= PlayerPos.x + PLAYER_RADIUS &&
+						PlayerPos.x <= bombPos.x - BOMB_RADIUS)
+					{//BOXの-X面にぶつかったので座標の補正
+						PlayerPos.x += (bombPos.x - BOMB_RADIUS) - (PlayerPos.x + PLAYER_RADIUS);
+						PlayerVel.x *= -COE; //移動ベクトルの反転
+						hit = COLLISION_HIT::HIT_WALL_3;
+						touch = true;
+					}
+					else if (bombPos.x + BOMB_RADIUS >= PlayerPos.x - PLAYER_RADIUS &&
+						PlayerPos.x >= bombPos.x + BOMB_RADIUS)
+					{//BOXの+X面にぶつかった
+						PlayerPos.x += (bombPos.x + BOMB_RADIUS) - (PlayerPos.x - PLAYER_RADIUS);
+						PlayerVel.x *= -COE;
+						hit = COLLISION_HIT::HIT_WALL_1;
+						touch = true;
+					}
 				}
-				else if (bombPos.z + BOMB_RADIUS >= PlayerPos.z - PLAYER_RADIUS &&
-					PlayerPos.z >= bombPos.z + BOMB_RADIUS)
-				{//BOXの+Z面にぶつかった
-					PlayerPos.z += (bombPos.z + BOMB_RADIUS) - (PlayerPos.z - PLAYER_RADIUS);
-					PlayerVel.z *= -COE;
-					hit = COLLISION_HIT::HIT_WALL_2;
-					touch = true;
-				}
-			}
-		}
-		//地面として判定処理
-		else
-		{
-			if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
-				PlayerPos.z <= bombPos.z + BOMB_RADIUS)
-			{
-				if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
+				else if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
 					PlayerPos.x <= bombPos.x + BOMB_RADIUS)
 				{
-					if (bombPos.y - BOMB_RADIUS <= PlayerPos.y + PLAYER_RADIUS &&
-						PlayerPos.y <= bombPos.y - BOMB_RADIUS)
-					{//BOXの-X面にぶつかったので座標の補正
-						PlayerPos.y += (bombPos.y - BOMB_RADIUS) - (PlayerPos.y + PLAYER_RADIUS);
-						PlayerVel.y *= -COE; //移動ベクトルの反転
-						//hit = 
+					if (bombPos.z - BOMB_RADIUS <= PlayerPos.z + PLAYER_RADIUS &&
+						PlayerPos.z <= bombPos.z - BOMB_RADIUS)
+					{//BOXの-Z面にぶつかったので座標の補正
+						PlayerPos.z += (bombPos.z - BOMB_RADIUS) - (PlayerPos.z + PLAYER_RADIUS);
+						PlayerVel.z *= -COE; //移動ベクトルの反転
+						hit = COLLISION_HIT::HIT_WALL_0;
+						touch = true;
 					}
-					else if (BoxTop >= PlayerPos.y - PLAYER_RADIUS &&
-						PlayerPos.y >= BoxTop)
-					{//BOXの+X面にぶつかった
-						PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
-						PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
-						hit = COLLISION_HIT::HIT_GROUND;
-						if (PlayerJump == false)
-						{
-							PlayerJump = true;
+					else if (bombPos.z + BOMB_RADIUS >= PlayerPos.z - PLAYER_RADIUS &&
+						PlayerPos.z >= bombPos.z + BOMB_RADIUS)
+					{//BOXの+Z面にぶつかった
+						PlayerPos.z += (bombPos.z + BOMB_RADIUS) - (PlayerPos.z - PLAYER_RADIUS);
+						PlayerVel.z *= -COE;
+						hit = COLLISION_HIT::HIT_WALL_2;
+						touch = true;
+					}
+				}
+			}
+			//地面として判定処理
+			else
+			{
+				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
+					PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+				{
+					if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
+						PlayerPos.x <= bombPos.x + BOMB_RADIUS)
+					{
+						if (bombPos.y - BOMB_RADIUS <= PlayerPos.y + PLAYER_RADIUS &&
+							PlayerPos.y <= bombPos.y - BOMB_RADIUS)
+						{//BOXの-X面にぶつかったので座標の補正
+							PlayerPos.y += (bombPos.y - BOMB_RADIUS) - (PlayerPos.y + PLAYER_RADIUS);
+							PlayerVel.y *= -COE; //移動ベクトルの反転
+							//hit = 
+						}
+						else if (BoxTop >= PlayerPos.y - PLAYER_RADIUS &&
+							PlayerPos.y >= BoxTop)
+						{//BOXの+X面にぶつかった
+							PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
+							PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
+							hit = COLLISION_HIT::HIT_GROUND;
+							if (PlayerJump == false)
+							{
+								PlayerJump = true;
+							}
 						}
 					}
 				}
 			}
+
+			RunBomb[i].Runbombsource_SetTouch(touch);
+			pPlayer->SetPlayerJump(PlayerJump);
+			pPlayer->SetPlayerPosition(PlayerPos);
+			pPlayer->SetPlayerVelocity(PlayerVel);
+
 		}
 
-		RunBomb[i].Runbombsource_SetTouch(touch);
-		pPlayer->SetPlayerJump(PlayerJump);
-		pPlayer->SetPlayerPosition(PlayerPos);
-		pPlayer->SetPlayerVelocity(PlayerVel);
-
-	}
-	
-	for (int i = 0; i < BOMB_NUM_MAX; i++)
-	{
-		/*switch (Bomb[i].BombSource_GetState())
+		for (int i = 0; i < BOMB_NUM_MAX; i++)
 		{
-		case BOMB_NONE:
-		case BOMB_MAX:
-		default:
-			continue;
-			break;
-		}*/
-
-		bool	touch = false;
-		float BoxTop;	// BOXの+Y面の座標
-
-		XMFLOAT3 bombPos;
-
-
-		bombPos = FlowtBomb[i].Flowtbombsource_GetPosition();
-
-
-
-		switch (FlowtBomb[i].Flowtbombsource_GetState())
-		{
-
-		default:
-			BoxTop = bombPos.y + BOMB_RADIUS;	// 普通のBOX
-			break;
-		}
-
-		// 壁としての判定処理
-		if (bombPos.y - BOMB_RADIUS <= PlayerPos.y &&
-			PlayerPos.y <= BoxTop - 0.1f)
-		{
-			if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
-				PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+			/*switch (Bomb[i].BombSource_GetState())
 			{
-				if (bombPos.x - BOMB_RADIUS <= PlayerPos.x + PLAYER_RADIUS &&
-					PlayerPos.x <= bombPos.x - BOMB_RADIUS)
-				{//BOXの-X面にぶつかったので座標の補正
-					PlayerPos.x += (bombPos.x - BOMB_RADIUS) - (PlayerPos.x + PLAYER_RADIUS);
-					PlayerVel.x *= -COE; //移動ベクトルの反転
-					hit = COLLISION_HIT::HIT_WALL_3;
-					touch = true;
-				}
-				else if (bombPos.x + BOMB_RADIUS >= PlayerPos.x - PLAYER_RADIUS &&
-					PlayerPos.x >= bombPos.x + BOMB_RADIUS)
-				{//BOXの+X面にぶつかった
-					PlayerPos.x += (bombPos.x + BOMB_RADIUS) - (PlayerPos.x - PLAYER_RADIUS);
-					PlayerVel.x *= -COE;
-					hit = COLLISION_HIT::HIT_WALL_1;
-					touch = true;
-				}
+			case BOMB_NONE:
+			case BOMB_MAX:
+			default:
+				continue;
+				break;
+			}*/
+
+			bool	touch = false;
+			float BoxTop;	// BOXの+Y面の座標
+
+			XMFLOAT3 bombPos;
+
+
+			bombPos = FlowtBomb[i].Flowtbombsource_GetPosition();
+
+
+
+			switch (FlowtBomb[i].Flowtbombsource_GetState())
+			{
+
+			default:
+				BoxTop = bombPos.y + BOMB_RADIUS;	// 普通のBOX
+				break;
 			}
-			else if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
-				PlayerPos.x <= bombPos.x + BOMB_RADIUS)
+
+			// 壁としての判定処理
+			if (bombPos.y - BOMB_RADIUS <= PlayerPos.y &&
+				PlayerPos.y <= BoxTop - 0.1f)
 			{
-				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z + PLAYER_RADIUS &&
-					PlayerPos.z <= bombPos.z - BOMB_RADIUS)
-				{//BOXの-Z面にぶつかったので座標の補正
-					PlayerPos.z += (bombPos.z - BOMB_RADIUS) - (PlayerPos.z + PLAYER_RADIUS);
-					PlayerVel.z *= -COE; //移動ベクトルの反転
-					hit = COLLISION_HIT::HIT_WALL_0;
-					touch = true;
+				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
+					PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+				{
+					if (bombPos.x - BOMB_RADIUS <= PlayerPos.x + PLAYER_RADIUS &&
+						PlayerPos.x <= bombPos.x - BOMB_RADIUS)
+					{//BOXの-X面にぶつかったので座標の補正
+						PlayerPos.x += (bombPos.x - BOMB_RADIUS) - (PlayerPos.x + PLAYER_RADIUS);
+						PlayerVel.x *= -COE; //移動ベクトルの反転
+						hit = COLLISION_HIT::HIT_WALL_3;
+						touch = true;
+					}
+					else if (bombPos.x + BOMB_RADIUS >= PlayerPos.x - PLAYER_RADIUS &&
+						PlayerPos.x >= bombPos.x + BOMB_RADIUS)
+					{//BOXの+X面にぶつかった
+						PlayerPos.x += (bombPos.x + BOMB_RADIUS) - (PlayerPos.x - PLAYER_RADIUS);
+						PlayerVel.x *= -COE;
+						hit = COLLISION_HIT::HIT_WALL_1;
+						touch = true;
+					}
 				}
-				else if (bombPos.z + BOMB_RADIUS >= PlayerPos.z - PLAYER_RADIUS &&
-					PlayerPos.z >= bombPos.z + BOMB_RADIUS)
-				{//BOXの+Z面にぶつかった
-					PlayerPos.z += (bombPos.z + BOMB_RADIUS) - (PlayerPos.z - PLAYER_RADIUS);
-					PlayerVel.z *= -COE;
-					hit = COLLISION_HIT::HIT_WALL_2;
-					touch = true;
-				}
-			}
-		}
-		//地面として判定処理
-		else
-		{
-			if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
-				PlayerPos.z <= bombPos.z + BOMB_RADIUS)
-			{
-				if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
+				else if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
 					PlayerPos.x <= bombPos.x + BOMB_RADIUS)
 				{
-					if (bombPos.y - BOMB_RADIUS <= PlayerPos.y + PLAYER_RADIUS &&
-						PlayerPos.y <= bombPos.y - BOMB_RADIUS)
-					{//BOXの-X面にぶつかったので座標の補正
-						PlayerPos.y += (bombPos.y - BOMB_RADIUS) - (PlayerPos.y + PLAYER_RADIUS);
-						PlayerVel.y *= -COE; //移動ベクトルの反転
-						//hit = 
+					if (bombPos.z - BOMB_RADIUS <= PlayerPos.z + PLAYER_RADIUS &&
+						PlayerPos.z <= bombPos.z - BOMB_RADIUS)
+					{//BOXの-Z面にぶつかったので座標の補正
+						PlayerPos.z += (bombPos.z - BOMB_RADIUS) - (PlayerPos.z + PLAYER_RADIUS);
+						PlayerVel.z *= -COE; //移動ベクトルの反転
+						hit = COLLISION_HIT::HIT_WALL_0;
+						touch = true;
 					}
-					else if (BoxTop >= PlayerPos.y - PLAYER_RADIUS &&
-						PlayerPos.y >= BoxTop)
-					{//BOXの+X面にぶつかった
-						PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
-						PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
-						hit = COLLISION_HIT::HIT_GROUND;
-						if (PlayerJump == false)
-						{
-							PlayerJump = true;
+					else if (bombPos.z + BOMB_RADIUS >= PlayerPos.z - PLAYER_RADIUS &&
+						PlayerPos.z >= bombPos.z + BOMB_RADIUS)
+					{//BOXの+Z面にぶつかった
+						PlayerPos.z += (bombPos.z + BOMB_RADIUS) - (PlayerPos.z - PLAYER_RADIUS);
+						PlayerVel.z *= -COE;
+						hit = COLLISION_HIT::HIT_WALL_2;
+						touch = true;
+					}
+				}
+			}
+			//地面として判定処理
+			else
+			{
+				if (bombPos.z - BOMB_RADIUS <= PlayerPos.z &&
+					PlayerPos.z <= bombPos.z + BOMB_RADIUS)
+				{
+					if (bombPos.x - BOMB_RADIUS <= PlayerPos.x &&
+						PlayerPos.x <= bombPos.x + BOMB_RADIUS)
+					{
+						if (bombPos.y - BOMB_RADIUS <= PlayerPos.y + PLAYER_RADIUS &&
+							PlayerPos.y <= bombPos.y - BOMB_RADIUS)
+						{//BOXの-X面にぶつかったので座標の補正
+							PlayerPos.y += (bombPos.y - BOMB_RADIUS) - (PlayerPos.y + PLAYER_RADIUS);
+							PlayerVel.y *= -COE; //移動ベクトルの反転
+							//hit = 
+						}
+						else if (BoxTop >= PlayerPos.y - PLAYER_RADIUS &&
+							PlayerPos.y >= BoxTop)
+						{//BOXの+X面にぶつかった
+							PlayerPos.y += (BoxTop)-(PlayerPos.y - PLAYER_RADIUS);
+							PlayerVel.y = PlayerVel.y * (-COE * 1.0f);
+							hit = COLLISION_HIT::HIT_GROUND;
+							if (PlayerJump == false)
+							{
+								PlayerJump = true;
+							}
 						}
 					}
 				}
 			}
+
+			FlowtBomb[i].Flowtbombsource_SetTouch(touch);
+			pPlayer->SetPlayerJump(PlayerJump);
+			pPlayer->SetPlayerPosition(PlayerPos);
+			pPlayer->SetPlayerVelocity(PlayerVel);
+
 		}
 
-		FlowtBomb[i].Flowtbombsource_SetTouch(touch);
-		pPlayer->SetPlayerJump(PlayerJump);
-		pPlayer->SetPlayerPosition(PlayerPos);
-		pPlayer->SetPlayerVelocity(PlayerVel);
-
 	}
-	
 
+	pPlayer->SetPlayerBombHave(bombHave);
 	return hit;  // ぶつかったかどうかを示す
 }
 
