6b8d32899afcdb0232f9e63de59a1220938116a5
Sooota7 <touya6881@gmail.com>
2026-02-10 11:11:22 +0900

    調整

--- bombUI.cpp
+++ bombUI.cpp
@@ -117,7 +117,7 @@ void BombUI::Draw()
 	const float SCREEN_WIDTH = (float)Direct3D_GetBackBufferWidth();
 	const float SCREEN_HEIGHT = (float)Direct3D_GetBackBufferHeight();
 
-	XMFLOAT3	position = XMFLOAT3(SCREEN_WIDTH * 0.8f, SCREEN_HEIGHT * 0.8f, 0.0f);
+	XMFLOAT3	position = XMFLOAT3(SCREEN_WIDTH * 0.9f, SCREEN_HEIGHT * 0.9f, 0.0f);
 	XMFLOAT2	holderSize = XMFLOAT2(200.0f, 200.0f);
 	XMFLOAT4	color = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 
